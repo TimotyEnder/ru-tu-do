94c56fa68890e313726bb687cbd3994c2ea46034
TimotyEnder <61980517+TimotyEnder@users.noreply.github.com>
2026-04-27 00:28:27 +0200

    implement UI

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,13 @@
 use crate::turing_machine::TuringMachine;
-
+use eframe::egui;
 mod turing_machine;
-
-fn main() {
-    let mut turing_mach = TuringMachine::new(3, &[2], &(0));
-    turing_mach.add_transition(0, 0, "C", "A", "r").unwrap();
-    turing_mach.add_transition(0, 0, "A", "C", "r").unwrap();
-    turing_mach.add_transition(0, 1, "B", "B", "l").unwrap();
-    turing_mach.add_transition(1, 1, "A", "A", "l").unwrap();
-    turing_mach.add_transition(1, 1, "C", "C", "l").unwrap();
-    turing_mach.add_transition(1, 2, "B", "B", "r").unwrap();
-    let (tape, accepted, final_head_position) = turing_mach.process_string_input("AACCCAA");
-    println!("\n{:?}", tape);
-    print!(
-        "final head position is on the {}th element\n",
-        final_head_position
-    );
-    print!("accepted? {}\n", accepted);
+struct App {
+    counter: i32,
 }
+impl Default for App {
+    fn default() -> Self {
+        Self { counter: 0 }
+    }
+}
+
+fn main() {}
